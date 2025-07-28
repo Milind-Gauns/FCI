da70bb843550129501f7d9bff392ca5b84c6cbe6
Ajinkya20014 <sawkarajinkya2001@gmail.com>
2025-07-28 23:11:44 +0530

    change done

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os, sys
 # ensure the folder containing app.py is on Python's import path
 sys.path.append(os.path.dirname(__file__))
 
-from simulation import run_simulation
+
 from matplotlib.backends.backend_pdf import PdfPages
 
 
